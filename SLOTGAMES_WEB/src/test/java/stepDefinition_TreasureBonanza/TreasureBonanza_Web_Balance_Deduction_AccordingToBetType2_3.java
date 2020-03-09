bfc59effc1616b758096d8dd0bf4e6c583ec638f
pavanysecit <pavan.kumar@ysecit.com>
2020-03-09 12:30:04 +0530

    dr

--- SLOTGAMES_WEB/src/test/java/stepDefinition_TreasureBonanza/TreasureBonanza_Web_Balance_Deduction_AccordingToBetType2_3.java
+++ SLOTGAMES_WEB/src/test/java/stepDefinition_TreasureBonanza/TreasureBonanza_Web_Balance_Deduction_AccordingToBetType2_3.java
@@ -1,7 +1,6 @@
 package stepDefinition_TreasureBonanza;
 
 import java.io.File;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -14,13 +13,12 @@ import org.sikuli.script.Finder;
 import org.sikuli.script.Match;
 import org.sikuli.script.Pattern;
 import org.sikuli.script.Screen;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class TreasureBonanza_Web_Balance_Deduction_AccordingToBetType2_3 {
-	WebDriver driver;
+WebDriver driver;
 	
 	@Given("^Chrome browser, valid URL, valid login details, Treasure Bonanza game, bet type as (\\d+)\\.(\\d+), bet value as TWO, balance and spin button$")
 	public void chrome_browser_valid_URL_valid_login_details_Treasure_Bonanza_game_bet_type_as_bet_value_as_TWO_balance_and_spin_button(int arg1, int arg2) throws Throwable {
@@ -50,62 +48,60 @@ public class TreasureBonanza_Web_Balance_Deduction_AccordingToBetType2_3 {
 		Pattern plus=new Pattern("E:/Sikuli Images/TreasureBonanza/plus.png");
 		Pattern spin=new Pattern("E:/Sikuli Images/TreasureBonanza/spin.png");
 
+		//clicking on Bet VALUE
+		screen.click(plus);
+		Thread.sleep(2000);
+		screen.click(pat1);
+		Thread.sleep(2000);
+		screen.click(spin);
+		Thread.sleep(6000);
 
-	  //clicking on Bet VALUE
-       screen.click(plus);
-       Thread.sleep(2000);
-       screen.click(pat1);
-       Thread.sleep(2000);
-       screen.click(spin);
-       Thread.sleep(6000);
-
-	
-	      //comparing the credit value should be 0.02
-	      Pattern credit1=new Pattern("E:/Sikuli Images/TreasureBonanza/creditvalue_2.png");
-	      Finder finder =new Finder(screen.capture().getImage());
-	      String ht = finder.find(credit1);
-	      double score=20;                
-	      System.out.println("the value of ht"+" "+ht);
-		  if(finder.hasNext())
-		  {
-		  Match m=finder.next();
-		  System.out.println("Match Found with: "+(m.getScore())*100+"%");
-		  score=(m.getScore())*100;
-		  System.out.println("Credit value comparision happened successfully. Test case passed");
-		  finder.destroy();  
-		  }         
-		  else    
-		  { 
-		  System.out.println("Comparision failed. Test case failed");         
-		  }
-		  System.out.println("Credit comparision value equals to: "+" "+score +"%");
-		  Assert.assertTrue(score > 97); 
-		  
-		  //comparing the balance after spinning should be deducted by 2 value
-	      Pattern pat=new Pattern("E:/Sikuli Images/TreasureBonanza/balance2_3.png");
-	      Finder finder1 =new Finder(screen.capture().getImage());
-	      String ht1 = finder1.find(pat);
-	      double score1=20;                
-	      System.out.println("the value of ht"+" "+ht1);
-	 	  if(finder1.hasNext())
-	 	  {
-	 	  Match m1=finder1.next();
-	 	  System.out.println("Match Found with: "+(m1.getScore())*100+"%");
-	 	  score1=(m1.getScore())*100;
-	 	  System.out.println("Comparision happened successfully. Test case passed");
-	 	  finder1.destroy();  
-	 	  }         
-	 	  else    
-	 	  { 
-	 	  System.out.println("Comparision failed. Test case failed");         
-	 	  }
-	 	  System.out.println("Comparision value equals to: "+" "+score1 +"%");
-	 	  Assert.assertTrue(score1 > 97);
+		//comparing the credit value should be 0.02
+		Pattern credit1=new Pattern("E:/Sikuli Images/TreasureBonanza/creditvalue_2.png");
+		Finder finder =new Finder(screen.capture().getImage());
+		String ht = finder.find(credit1);
+		double score=20;                
+		System.out.println("the value of ht"+" "+ht);
+		if(finder.hasNext())
+		{
+		Match m=finder.next();
+		System.out.println("Match Found with: "+(m.getScore())*100+"%");
+		score=(m.getScore())*100;
+		System.out.println("Credit value comparision happened successfully. Test case passed");
+		finder.destroy();  
+		}         
+		else    
+		{ 
+		System.out.println("Comparision failed. Test case failed");         
+		}
+		System.out.println("Credit comparision value equals to: "+" "+score +"%");
+		Assert.assertTrue(score > 97); 
+				  
+		//comparing the balance after spinning should be deducted by 2 value
+		Pattern pat=new Pattern("E:/Sikuli Images/TreasureBonanza/balance2_3.png");
+		Finder finder1 =new Finder(screen.capture().getImage());
+		String ht1 = finder1.find(pat);
+		double score1=20;                
+		System.out.println("the value of ht"+" "+ht1);
+		if(finder1.hasNext())
+		{
+		Match m1=finder1.next();
+		System.out.println("Match Found with: "+(m1.getScore())*100+"%");
+		score1=(m1.getScore())*100;
+		System.out.println("Comparision happened successfully. Test case passed");
+		finder1.destroy();  
+		}         
+		else    
+		{ 
+		System.out.println("Comparision failed. Test case failed");         
+		}
+		System.out.println("Comparision value equals to: "+" "+score1 +"%");
+		Assert.assertTrue(score1 > 97);
 	}
 
 	@Then("^Current Balance should get deducted by TWO as bet type is selected as (\\d+)\\.(\\d+) & bet value as (\\d+) in Treasure Bonanza game$")
 	public void current_Balance_should_get_deducted_by_TWO_as_bet_type_is_selected_as_bet_value_as_in_Treasure_Bonanza_game(int arg1, int arg2, int arg3) throws Throwable {
-		 Thread.sleep(3000);
-		 driver.quit();
+		Thread.sleep(3000);
+		driver.quit();
 	}
 }
